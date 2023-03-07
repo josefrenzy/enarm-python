3ed3bddccd3ace0b3663fee8600d8a1f1eac2bb1
josefrenzy <guzmangordillojose@gmail.com>
2023-03-06 23:24:41 -0600

    add all files

--- rest_srv.py
+++ rest_srv.py
@@ -4,10 +4,12 @@ from flask_restx import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+
 @api.route('/hello')
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001, host='0.0.0.0')
